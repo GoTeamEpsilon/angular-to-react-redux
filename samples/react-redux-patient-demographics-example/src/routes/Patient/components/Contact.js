6dffa9208668a74ce5e544ec66f7d419330d0f9d
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2017-02-07 21:45:39 -0600

    contact info component

---
+++ samples/react-redux-patient-demographics-example/src/routes/Patient/components/Contact.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import React, {Component} from 'react';
+
+class Contact extends Component {
+  
+  
+  render () {
+    return (
+      <div>
+        <table className="table">
+          <tr>
+            <td><strong>Name:</strong> Jane Doe</td>
+            <td><strong>Relation:</strong>Mother</td>
+          </tr>
+          <tr>
+            <td><strong>Address:</strong>123 Foobar Ln</td>
+            <td><strong>Phone:</strong>CoolCity</td>
+          </tr>
+          <tr>
+            <td><strong>City:</strong>CoolCity</td>
+            <td><strong>Postal:</strong>12345</td>
+          </tr>
+          <tr>
+            <td><strong>State:</strong>Texas</td>
+            <td><strong>Country:</strong>US</td>
+          </tr>
+          <tr>
+            <td><strong>Email:</strong>bar@foo.com</td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Patient;
+
+
