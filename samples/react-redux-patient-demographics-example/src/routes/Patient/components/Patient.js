895220cb713d4fc3ab66aed1336903044e87744c
Matthew Vita <matthewvita@live.com>
2017-02-12 13:03:22 -0500

    correct container/component scaffolding

--- samples/react-redux-patient-demographics-example/src/routes/Patient/components/Patient.js
+++ samples/react-redux-patient-demographics-example/src/routes/Patient/components/Patient.js
@@ -1,30 +1,69 @@
-import React from 'react';
-
+import React from 'react'
+import { browserHistory } from 'react-router';
 
 class Patient extends React.Component {
-  componentDidMount () {
-    const isValid = this.props.determineIfRouteIsValid(
-      this.props.pid
-    );
+  constructor() {
+    super()
 
-    if (isValid) {
-      this.props.setPatientInContext(this.props.pid);
-    } else {
-      this.props.redirectToSamplePatient();
+    this.TABS = {
+      BASIC: 'basic',
+      CONTACTS: 'contacts',
+    }
+
+    this.state = {
+      tab: this.TABS.BASIC,
     }
   }
 
-  render () {
+  setPatientInContext() {
+    this.props.setPatientInContext(this.props.routeParams.pid)
+  }
+
+  determineIfRouteIsValid() {
+    return this.props.routeParams.pid
+  }
+
+  componentDidMount() {
+    if (!this.determineIfRouteIsValid()) {
+      browserHistory.push('/patient/1337');
+    } else {
+      this.setPatientInContext()
+    }
+  }
+
+  mockedTab() {
+    alert('This tab is just here for completeness. The real tabs are basic and contacts')
+  }
+
+  changeTab(newTab) {
+    console.debug(`Setting tab to ${newTab}`)
+    this.setState({ tab: newTab, })
+  }
+
+  render() {
     return (
       <div>
-        <h4>Patient</h4>
-        <hr/>
-        
+        <h3 className={this.props.isLoading ? '' : 'hidden'}>Loading...</h3>
 
-          
+        <div className={this.props.isLoading ? 'hidden' : ''}>
+          <div>
+            <ul className="nav nav-tabs">
+              <li className={this.state.tab == this.TABS.BASIC ? 'active' : ''}>
+                <a onClick={() => this.changeTab(this.TABS.BASIC)}>Basic</a>
+              </li>
+              <li className={this.state.tab == this.TABS.CONTACTS ? 'active' : ''}>
+                <a onClick={() => this.changeTab(this.TABS.CONTACTS)}>Contacts</a>
+              </li>
+              <li><a onClick={this.mockedTab}>Choices</a></li>
+              <li><a onClick={this.mockedTab}>Employer</a></li>
+              <li><a onClick={this.mockedTab}>Stats</a></li>
+              <li><a onClick={this.mockedTab}>Misc</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default Patient;
+export default Patient
--- samples/react-redux-patient-demographics-example/src/routes/Patient/containers/PatientContainer.js
+++ samples/react-redux-patient-demographics-example/src/routes/Patient/containers/PatientContainer.js
@@ -1,31 +1,15 @@
-import { connect } from 'react-redux';
-import { setPatientInContext } from '../modules/patient';
-import { browserHistory } from 'react-router';
-import Patient from '../components/Patient';
+import { connect } from 'react-redux'
+import { setPatientInContext } from '../modules/patient'
+import { browserHistory } from 'react-router'
+import Patient from '../components/Patient'
 
+const mapDispatchToProps = {
+  setPatientInContext,
+}
 
-const _setPatientInContext = (pid) => {
-  setPatientInContext(pid);
-};
+const mapStateToProps = (state) => ({
+  patientInContext: state.patient.patientInContext,
+  isLoading: state.patient.isFetching,
+})
 
-const _determineIfRouteIsValid = (pid) => {
-  return !!(pid);
-};
-
-const _redirectToSamplePatient = () => {
-  browserHistory.push('/patient/1337');
-};
-
-const mapDispatchToProps = (dispatch, props) => {
-  return {
-    setPatientInContext: _setPatientInContext,
-    determineIfRouteIsValid: _determineIfRouteIsValid,
-    redirectToSamplePatient: _redirectToSamplePatient
-  };
-};
-
-const mapStateToProps = (state, props) => ({
-  pid: props.routeParams.pid
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Patient);
+export default connect(mapStateToProps, mapDispatchToProps)(Patient)
