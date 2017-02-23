311a62b964a7c718efb22eea7172a5bed6184eec
Matthew Vita <matthewvita@live.com>
2017-02-22 23:22:56 -0500

    more progress on basic form using Daniels advice

---
+++ samples/react-redux-patient-demographics-example/src/common/Formatters.js
@@ -0,0 +1,30 @@
+export const telephoneFormat = (input) => {
+  if (!input) {
+    return
+  }
+
+  if(input.toString().length === 10 ) {
+    var outputTempString = input.toString()
+    var outputString = outputTempString.substr(0,3) + '-' + outputTempString.substr(3,3) + '-' + outputTempString.substr(6,4)
+    return outputString
+  } else {
+    return input.toString()
+  }
+}
+
+export const socialSecurityFormat = (input) => {
+  if (!input) {
+    return
+  }
+
+  var outputString
+
+  if (input.length < 9) {
+    return input
+  } else {
+    var outputTempString = input.toString()
+    outputString = outputTempString.substr(0,3) + '-' + outputTempString.substr(3,2) + '-' + outputTempString.substr(5,4)
+  }
+
+  return outputString;
+}
--- samples/react-redux-patient-demographics-example/src/routes/Patient/Demographics/Basic/BasicComponent.js
+++ samples/react-redux-patient-demographics-example/src/routes/Patient/Demographics/Basic/BasicComponent.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import { telephoneFormat, socialSecurityFormat } from '../../../../common/Formatters'
+
+require('react-datepicker/dist/react-datepicker.css')
 
 class Basic extends React.Component {
   constructor() {
     super()
     this.state = {
       showForm: false,
-      name: ''
+      dob: moment()
     }
+    this.handleInputChange = this.handleInputChange.bind(this)
     this.handleEdit = this.handleEdit.bind(this)
   }
 
@@ -22,8 +28,18 @@ class Basic extends React.Component {
   }
 
   handleInputChange(event) {
-    const value = event.target.value
-    const name = event.target.name
+    if (event.hasOwnProperty('target') && event.target.hasOwnProperty('name')) {
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    } else {
+      // Assuming it is the date timer picker
+      this.setState({
+        dob: event
+      })
+    }
+
+    console.log(this.state)
   }
 
   render() {
@@ -36,7 +52,7 @@ class Basic extends React.Component {
               <td><strong>DOB:</strong> {this.props.info.dob}</td>
             </tr>
             <tr>
-              <td><strong>S.S.:</strong> {this.props.info.ss}</td>
+              <td><strong>S.S.:</strong> {socialSecurityFormat(this.props.info.ss)}</td>
               <td><strong>Martial Status:</strong> {this.props.info.martialStatus}</td>
             </tr>
             <tr>
@@ -52,7 +68,7 @@ class Basic extends React.Component {
               <td><strong>Country:</strong> {this.props.info.country}</td>
             </tr>
             <tr>
-              <td><strong>Phone:</strong> {this.props.info.phone}</td>
+              <td><strong>Phone:</strong> {telephoneFormat(this.props.info.phone)}</td>
               <td><strong>Email:</strong> {this.props.info.email}</td>
             </tr>
             <tr>
@@ -71,7 +87,74 @@ class Basic extends React.Component {
             <tr>
               <td>
                 <strong>Name:</strong>
-                <input type="text" value={this.props.info.name} onChange={this.handleInputChange.bind(this)} name="name" />
+                <input type="text" onChange={this.handleInputChange} name="name" />
+              </td>
+              <td>
+                <strong>DOB:</strong>
+                <DatePicker selected={this.state.dob} onChange={this.handleInputChange} name="dob" />;
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>SSN:</strong>
+                <input type="text" onChange={this.handleInputChange} name="ssn" />
+              </td>
+              <td>
+                <strong>Martial Status:</strong>
+                <input type="text" onChange={this.handleInputChange} name="martialStatus" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Gender:</strong>
+                <select onChange={this.handleInputChange} name="gender">
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </td>
+              <td>
+                <strong>Address:</strong>
+                <input type="text" onChange={this.handleInputChange} name="address" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>City:</strong>
+                <input type="text" onChange={this.handleInputChange} name="city" />
+              </td>
+              <td>
+                <strong>Postal:</strong>
+                <input type="text" onChange={this.handleInputChange} name="city" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>State:</strong>
+                <input type="text" onChange={this.handleInputChange} name="state" />
+              </td>
+              <td>
+                <strong>Country:</strong>
+                <input type="text" onChange={this.handleInputChange} name="country" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Phone:</strong>
+                <input type="text" onChange={this.handleInputChange} name="phone" />
+              </td>
+              <td>
+                <strong>Email:</strong>
+                <input type="text" onChange={this.handleInputChange} name="email" />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Billing Note:</strong>
+                <input type="text" onChange={this.handleInputChange} name="billingNote" />
+              </td>
+              <td>
+                <strong>Other Note</strong>
+                <input type="text" onChange={this.handleInputChange} name="otherNote" />
               </td>
             </tr>
           </table>
